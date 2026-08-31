a4b91a8733001c95db9767553fdc7dc606dd8ea0
Pblackberry <130101487+Pblackberry@users.noreply.github.com>
2026-08-30 21:52:49 -0500

    Revert "Feat/get catalog endpoint"

--- app/dependencies.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.db.database import get_db
-from app.repositories.catalog_respository import CatalogRepository
-
-
-def get_cobot_repository(db: AsyncSession = Depends(get_db)) -> CatalogRepository:
-    return CatalogRepository(db)
